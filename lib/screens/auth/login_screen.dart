74952f8999541919e3bc903126792c2035b6dcf7
Lans12345 <olanalans12345@gmail.com>
2023-02-14 12:38:08 +0800

    fixed overflow

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -13,78 +13,83 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/background.png'),
-                fit: BoxFit.cover)),
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 150,
-              ),
-              Image.asset(
-                'assets/images/aquarius.png',
-                width: 280,
-              ),
-              const SizedBox(
-                height: 120,
-              ),
-              TextFieldWidget(
-                  label: 'Phone Number', controller: phoneController),
-              TextFieldWidget(
-                  isObscure: true,
-                  label: 'Password',
-                  controller: passwordController),
-              const SizedBox(
-                height: 20,
-              ),
-              ButtonWidget(
-                  label: 'Log In',
-                  onPressed: (() {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (context) => const HomeScreen()));
-                  }),
-                  buttonColor: primary),
-              TextButton(
-                  onPressed: (() {}),
-                  child: TextRegular(
-                      fw: FontWeight.w700,
-                      text: 'Forgot Password?',
-                      fontSize: 15,
-                      color: Colors.white)),
-              const Expanded(
-                child: SizedBox(
+      body: SingleChildScrollView(
+        child: Container(
+          height: 800,
+          width: 500,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.cover)),
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 150,
+                ),
+                Image.asset(
+                  'assets/images/aquarius.png',
+                  width: 280,
+                ),
+                const SizedBox(
+                  height: 120,
+                ),
+                TextFieldWidget(
+                    label: 'Phone Number', controller: phoneController),
+                TextFieldWidget(
+                    isObscure: true,
+                    label: 'Password',
+                    controller: passwordController),
+                const SizedBox(
                   height: 20,
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextRegular(
-                      fw: FontWeight.w700,
-                      text: "Don't have an account?",
-                      fontSize: 14,
-                      color: Colors.white),
-                  const SizedBox(
-                    width: 10,
+                ButtonWidget(
+                    label: 'Log In',
+                    onPressed: (() {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                    }),
+                    buttonColor: primary),
+                TextButton(
+                    onPressed: (() {}),
+                    child: TextRegular(
+                        fw: FontWeight.w700,
+                        text: 'Forgot Password?',
+                        fontSize: 15,
+                        color: Colors.white)),
+                const Expanded(
+                  child: SizedBox(
+                    height: 20,
                   ),
-                  TextButton(
-                      onPressed: (() {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => SignupScreen()));
-                      }),
-                      child: TextBold(
-                          text: "Sign Up", fontSize: 18, color: Colors.white)),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-            ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextRegular(
+                        fw: FontWeight.w700,
+                        text: "Don't have an account?",
+                        fontSize: 14,
+                        color: Colors.white),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    TextButton(
+                        onPressed: (() {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => SignupScreen()));
+                        }),
+                        child: TextBold(
+                            text: "Sign Up",
+                            fontSize: 18,
+                            color: Colors.white)),
+                  ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -30,123 +30,124 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/background.png'),
-                fit: BoxFit.cover)),
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 75,
-              ),
-              Image.asset(
-                'assets/images/aquarius.png',
-                width: 280,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFieldWidget(
-                  label: 'Phone Number', controller: phoneController),
-              TextFieldWidget(label: 'First Name', controller: phoneController),
-              TextFieldWidget(label: 'Last Name', controller: phoneController),
-              TextFieldWidget(
-                  isObscure: true,
-                  label: 'Password',
-                  controller: passwordController),
-              TextFieldWidget(
-                  label: 'Fishpond Size (Ha)', controller: phoneController),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 40, right: 40, top: 10, bottom: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextRegular(
-                        text: 'Domesticated Aquatic Organism',
-                        fontSize: 12,
-                        color: Colors.white),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 2, 20, 2),
-                        child: DropdownButton(
-                          underline: Container(color: Colors.transparent),
-                          iconEnabledColor: Colors.black,
-                          isExpanded: true,
-                          value: dropDownValue,
-                          items: [
-                            for (int i = 0; i < dropItems.length; i++)
-                              DropdownMenuItem(
-                                onTap: () {
-                                  org = dropItems[i];
-                                },
-                                value: i,
-                                child: Center(
-                                    child: Row(children: [
-                                  Text(dropItems[i],
-                                      style: const TextStyle(
-                                        fontFamily: 'QBold',
-                                        color: Colors.grey,
-                                      ))
-                                ])),
-                              ),
-                          ],
-                          onChanged: (value) {
-                            setState(() {
-                              dropDownValue = int.parse(value.toString());
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Container(
+          height: 800,
+          width: 500,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.cover)),
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 75,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ButtonWidget(
-                  label: 'Sign Up', onPressed: (() {}), buttonColor: primary),
-              const Expanded(
-                child: SizedBox(
+                Image.asset(
+                  'assets/images/aquarius.png',
+                  width: 280,
+                ),
+                const SizedBox(
                   height: 20,
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextRegular(
-                      fw: FontWeight.w700,
-                      text: "Already have an account?",
-                      fontSize: 14,
-                      color: Colors.white),
-                  const SizedBox(
-                    width: 10,
+                TextFieldWidget(
+                    label: 'Phone Number', controller: phoneController),
+                TextFieldWidget(
+                    label: 'First Name', controller: firstNameController),
+                TextFieldWidget(
+                    label: 'Last Name', controller: lastNameController),
+                TextFieldWidget(
+                    isObscure: true,
+                    label: 'Password',
+                    controller: passwordController),
+                TextFieldWidget(
+                    label: 'Fishpond Size (Ha)',
+                    controller: passwordController),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 40, right: 40, top: 10, bottom: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextRegular(
+                          text: 'Domesticated Aquatic Organism',
+                          fontSize: 12,
+                          color: Colors.white),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 2, 20, 2),
+                          child: DropdownButton(
+                            underline: Container(color: Colors.transparent),
+                            iconEnabledColor: Colors.black,
+                            isExpanded: true,
+                            value: dropDownValue,
+                            items: [
+                              for (int i = 0; i < dropItems.length; i++)
+                                DropdownMenuItem(
+                                  onTap: () {
+                                    org = dropItems[i];
+                                  },
+                                  value: i,
+                                  child: Center(
+                                      child: Row(children: [
+                                    Text(dropItems[i],
+                                        style: const TextStyle(
+                                          fontFamily: 'QBold',
+                                          color: Colors.grey,
+                                        ))
+                                  ])),
+                                ),
+                            ],
+                            onChanged: (value) {
+                              setState(() {
+                                dropDownValue = int.parse(value.toString());
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  TextButton(
-                      onPressed: (() {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => LoginScreen()));
-                      }),
-                      child: TextBold(
-                          text: "Log In", fontSize: 18, color: Colors.white)),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-            ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ButtonWidget(
+                    label: 'Sign Up', onPressed: (() {}), buttonColor: primary),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextRegular(
+                        fw: FontWeight.w700,
+                        text: "Already have an account?",
+                        fontSize: 14,
+                        color: Colors.white),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    TextButton(
+                        onPressed: (() {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => LoginScreen()));
+                        }),
+                        child: TextBold(
+                            text: "Log In", fontSize: 18, color: Colors.white)),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
