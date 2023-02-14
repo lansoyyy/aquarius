d878477039d92aa38f749d7f4145804617b95216
Lans12345 <olanalans12345@gmail.com>
2023-02-14 12:02:51 +0800

    added splash screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,78 @@
+import 'dart:async';
+
+import 'package:aquarius/screens/auth/login_screen.dart';
+import 'package:aquarius/widget/text_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(const Duration(seconds: 5), () async {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const LoginScreen()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.cover)),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Expanded(
+                child: SizedBox(
+                  height: 20,
+                ),
+              ),
+              Image.asset(
+                'assets/images/logo.png',
+                height: 80,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset(
+                'assets/images/aquarius.png',
+                width: 280,
+              ),
+              const SizedBox(
+                height: 300,
+              ),
+              TextRegular(
+                  text: 'University of Cebu Lapu-Lapu and Mandaue',
+                  fontSize: 15,
+                  color: Colors.white),
+              const SizedBox(
+                height: 10,
+              ),
+              TextRegular(
+                  text: 'Amorres, Areglo, Basmayor, Gonzales, Puerto',
+                  fontSize: 12,
+                  color: Colors.white60),
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widget/text_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class TextRegular extends StatelessWidget {
+  late String text;
+  late double fontSize;
+  late Color color;
+  late FontWeight fw;
+
+  TextRegular(
+      {required this.text,
+      required this.fontSize,
+      required this.color,
+      this.fw = FontWeight.normal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontSize: fontSize,
+          color: color,
+          fontFamily: 'QRegular',
+          fontWeight: fw),
+    );
+  }
+}
+
+class TextBold extends StatelessWidget {
+  late String text;
+  late double fontSize;
+  late Color color;
+
+  TextBold({
+    required this.text,
+    required this.fontSize,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        color: color,
+        fontFamily: 'QBold',
+      ),
+    );
+  }
+}
