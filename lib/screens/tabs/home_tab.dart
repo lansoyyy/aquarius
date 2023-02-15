f321ee2ff5003189ca068f654f8c516099195fc3
Lans12345 <olanalans12345@gmail.com>
2023-02-15 13:43:41 +0800

    added home tab

--- lib/screens/tabs/home_tab.dart
+++ lib/screens/tabs/home_tab.dart
@@ -1,3 +1,5 @@
+import 'package:aquarius/utils/colors.dart';
+import 'package:aquarius/widget/text_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
@@ -5,6 +7,134 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Container(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              TextBold(text: 'WELCOME', fontSize: 48, color: Colors.white),
+              TextRegular(text: 'John Doe!', fontSize: 22, color: Colors.white),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                    elevation: 3,
+                    child: Container(
+                      width: 180,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/temp.png', height: 100),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextRegular(
+                              text: 'Temperature',
+                              fontSize: 18,
+                              color: secondary,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                    elevation: 3,
+                    child: Container(
+                      width: 180,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/ph-level.png',
+                                height: 100),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextRegular(
+                              text: 'pH Level',
+                              fontSize: 18,
+                              color: secondary,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                    elevation: 3,
+                    child: Container(
+                      width: 180,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/dissolved-oxygen.png',
+                                height: 80),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextRegular(
+                              text: 'Dissolved\nOxygen',
+                              fontSize: 18,
+                              color: secondary,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
