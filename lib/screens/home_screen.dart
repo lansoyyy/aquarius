3d395cd870d7bf2c473acf77bef235a0887a5658
Lans12345 <olanalans12345@gmail.com>
2023-02-15 13:54:42 +0800

    wip data tab

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _index = 0;
 
-  final tabs = [const HomeTab(), const DataTab(), const SettingsTab()];
+  final tabs = [const HomeTab(), DataTab(), const SettingsTab()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/screens/tabs/data_tab.dart
+++ lib/screens/tabs/data_tab.dart
@@ -1,10 +1,123 @@
+import 'package:aquarius/widget/text_widget.dart';
 import 'package:flutter/material.dart';
 
-class DataTab extends StatelessWidget {
-  const DataTab({super.key});
+class DataTab extends StatefulWidget {
+  @override
+  State<DataTab> createState() => _DataTabState();
+}
+
+class _DataTabState extends State<DataTab> {
+  var dropItems = [
+    ' Today',
+    'Yesterday',
+    '2 Days Ago',
+    'Last Week ',
+    'Last Month'
+  ];
+
+  var dropValue = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Container(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextRegular(text: 'Show', fontSize: 16, color: Colors.white),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: DropdownButton(
+                        underline: const SizedBox(),
+                        value: dropValue,
+                        items: [
+                          for (int i = 0; i < dropItems.length; i++)
+                            DropdownMenuItem(
+                              value: i,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(50, 2, 50, 2),
+                                child: TextRegular(
+                                    text: dropItems[i],
+                                    fontSize: 14,
+                                    color: Colors.black),
+                              ),
+                            ),
+                        ],
+                        onChanged: ((value) {
+                          setState(() {
+                            dropValue = int.parse(value.toString());
+                          });
+                        })),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextRegular(text: 'pH Level', fontSize: 18, color: Colors.white),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                child: Card(
+                  elevation: 3,
+                  child: Container(
+                    height: 150,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              TextRegular(
+                  text: 'Temperature', fontSize: 18, color: Colors.white),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                child: Card(
+                  elevation: 3,
+                  child: Container(
+                    height: 150,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              TextRegular(
+                  text: 'Dissolved Oxygen', fontSize: 18, color: Colors.white),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                child: Card(
+                  elevation: 3,
+                  child: Container(
+                    height: 150,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
