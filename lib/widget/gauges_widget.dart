177129152f5af75df59030fbcd2087625e33e1dc
Lans12345 <olanalans12345@gmail.com>
2023-02-18 11:55:30 +0800

    change gauge

--- lib/widget/gauges_widget.dart
+++ lib/widget/gauges_widget.dart
@@ -12,31 +12,25 @@ class GaugeChart extends StatelessWidget {
         enableLoadingAnimation: true,
         axes: <RadialAxis>[
           RadialAxis(
+            showLabels: false,
+            showTicks: false,
             startAngle: 180,
-            endAngle: 1.8,
-            radiusFactor: 0.9,
-            minimum: 0,
-            maximum: 4.8,
-            ranges: <GaugeRange>[
-              GaugeRange(
-                  startValue: 0, endValue: 1.0, color: const Color(0xffB9F3E4)),
-              GaugeRange(
-                  startValue: 1.0,
-                  endValue: 2.5,
-                  color: const Color(0xffB9F3E4)),
-              GaugeRange(
-                  startValue: 2.5,
-                  endValue: 4.8,
-                  color: const Color(0xffB9F3E4)),
-            ],
-            pointers: const <GaugePointer>[NeedlePointer(value: 2.72)],
+            endAngle: 0,
+            radiusFactor: 0.8,
+            canScaleToFit: true,
+            axisLineStyle: const AxisLineStyle(
+              thickness: 0.1,
+              color: Color.fromARGB(30, 0, 169, 181),
+              thicknessUnit: GaugeSizeUnit.factor,
+              cornerStyle: CornerStyle.startCurve,
+            ),
             annotations: <GaugeAnnotation>[
               const GaugeAnnotation(
                 angle: 90,
-                positionFactor: 0.5,
                 widget: Text(
                   '27.18',
                   style: TextStyle(
+                      fontFamily: 'QBold',
                       fontSize: 32,
                       fontWeight: FontWeight.bold,
                       color: Colors.white),
@@ -44,12 +38,9 @@ class GaugeChart extends StatelessWidget {
               ),
               GaugeAnnotation(
                 angle: 90,
-                positionFactor: 1.75,
+                positionFactor: 1.50,
                 widget: Column(
                   children: const [
-                    SizedBox(
-                      height: 20,
-                    ),
                     Text(
                       'Optimum at',
                       style: TextStyle(
@@ -66,11 +57,34 @@ class GaugeChart extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                           color: Colors.white),
                     ),
+                    SizedBox(
+                      height: 20,
+                    ),
                   ],
                 ),
               ),
             ],
-          ),
+            ranges: [
+              GaugeRange(
+                  startValue: 0, endValue: 1.0, color: const Color(0xffB9F3E4)),
+              GaugeRange(
+                  startValue: 1.0,
+                  endValue: 2.5,
+                  color: const Color(0xffB9F3E4)),
+              GaugeRange(
+                  startValue: 2.5,
+                  endValue: 4.8,
+                  color: const Color(0xffB9F3E4)),
+            ],
+            pointers: const <GaugePointer>[
+              RangePointer(
+                  color: Color.fromARGB(255, 192, 236, 225),
+                  value: 100,
+                  width: 0.1,
+                  sizeUnit: GaugeSizeUnit.factor,
+                  cornerStyle: CornerStyle.bothCurve),
+            ],
+          )
         ],
       ),
     );
