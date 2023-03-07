f554a8a230d9ee6465ea639905eaca7788ad72c0
Lans12345 <olanalans12345@gmail.com>
2023-03-07 17:26:05 +0800

    added logout func

--- lib/screens/tabs/settings_tab.dart
+++ lib/screens/tabs/settings_tab.dart
@@ -1,3 +1,4 @@
+import 'package:aquarius/screens/auth/login_screen.dart';
 import 'package:aquarius/utils/colors.dart';
 import 'package:aquarius/widget/button_widget.dart';
 import 'package:aquarius/widget/change_pass_dialog.dart';
@@ -16,6 +17,51 @@ class SettingsTab extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: (() {
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        title: const Text(
+                          'Logout Confirmation',
+                          style: TextStyle(
+                              fontFamily: 'QBold', fontWeight: FontWeight.bold),
+                        ),
+                        content: const Text(
+                          'Are you sure you want to Logout?',
+                          style: TextStyle(fontFamily: 'QRegular'),
+                        ),
+                        actions: <Widget>[
+                          MaterialButton(
+                            onPressed: () => Navigator.of(context).pop(true),
+                            child: const Text(
+                              'Close',
+                              style: TextStyle(
+                                  fontFamily: 'QRegular',
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          MaterialButton(
+                            onPressed: () async {
+                              Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const LoginScreen()));
+                            },
+                            child: const Text(
+                              'Continue',
+                              style: TextStyle(
+                                  fontFamily: 'QRegular',
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ));
+            }),
+            icon: const Icon(Icons.logout),
+          ),
+        ],
         backgroundColor: secondary,
         title: TextBold(text: 'PROFILE', fontSize: 18, color: Colors.white),
         centerTitle: true,
